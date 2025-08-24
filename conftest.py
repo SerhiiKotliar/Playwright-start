9dd1f926ae8ff4569911a889b3e9fe31c1c332ee
Kotliar Serhii <arecserg@gmail.com>
2025-08-24 11:47:38 +0300

    редакция вставлены формы и фикстуры

--- conftest.py
+++ conftest.py
@@ -2,12 +2,20 @@ import pytest
 # from selenium import webdriver
 from main import get_user_input
 from playwright.sync_api import Page
+from playwright.sync_api import sync_playwright
 
 
 @pytest.fixture(scope="session")
 def user_data():
     """Фікстура, яка перед запуском тестів показує форму і повертає введені дані"""
     return get_user_input()
+
+# @pytest.fixture(scope="session")
+# def browser():
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=False)
+#         yield browser
+#         browser.close()
 
 @pytest.fixture(scope="function")
 def page_open(page: Page, user_data):
--- test_main.py
+++ test_main.py
@@ -5,10 +5,25 @@ from playwright.sync_api import Page, expect
 from conftest import page_open
 
 
-def test_first(page: Page, page_open):
+# def test_first(page: Page, page_open):
     # page.goto('https://www.google.com/')
-    logo = page_open.locator("img.logo_image")  # знаходимо <img> з класом
-    expect(logo).to_be_visible()  # перевіряємо, що зображення видно
+    # logo = page_open.locator("img.logo_image")  # знаходимо <img> з класом
+    # expect(logo).to_be_visible()  # перевіряємо, що зображення видно
+    # entrance = page_open.locator('xpath=(//*[@id="content"]/div/ol/li[5]/a)')
+    # entrance.click()
+    # choose = page_open.locator('xpath=(//*[@id="id_choose_language"]')
+
+
+
+
+    # choose.click()
+    # choose_lang = page_open.locator('xpath=(//*[@id="id_choose_language"]/option[2]')
+    # expect(choose_lang).to_have_text('Python')
+    # choose_lang.click()
+    # submit = page_open.locator('xpath=(//*[@id="submit-id-submit"]')
+    # submit.click()
+    # expect(page_open).to_have_text('You selected Python')
+
     # expect(page_open.locator("site_header")).to_be_visible()
     # expect(page_open).to_have_title('Google')
     # mail_link = page.get_by_role('link', name='mail')
@@ -18,6 +33,18 @@ def test_first(page: Page, page_open):
     # search_button = page.locator('xpath=(//*[@name="btnK"])[2]')
     # search_button.click()
     # expect(page).to_have_title(re.compile('cat'))
+
+def test_example(page: Page, page_open, browser) -> None:
+    logo = page_open.locator("img.logo_image")  # знаходимо <img> з класом
+    expect(logo).to_be_visible()  # перевіряємо, що зображення видно
+    expect(page_open.get_by_role("link", name="Select input")).to_be_visible()
+    page_open.get_by_role("link", name="Select input").click()
+    expect(page.get_by_label("Choose language*")).to_be_visible()
+    page.get_by_label("Choose language*").select_option(label="Python")
+    page.get_by_role("button", name="Submit").click()
+    expect(page.get_by_text("You selected Python")).to_be_visible()
+    page.get_by_text("You selected Python").screenshot(path="screenshots/Python.png")
+
 
 
 @pytest.mark.skip('Site doesn\'t work')
