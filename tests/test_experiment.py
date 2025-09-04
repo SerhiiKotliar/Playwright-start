ba03fe61f52eb260333a8090bdaebd637db9e72f
Kotliar Serhii <arecserg@gmail.com>
2025-09-04 20:32:30 +0300

    позитивы и негативы 8

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -1,78 +1,32 @@
 import allure
 import pytest
 from playwright.sync_api import expect
-
-# from conftest import user_data
 from helper import debug
+import re
+from playwright.sync_api import Page, expect
 
-@allure.title("Позитивний тест: поле 'First Name*' відображається")
-@pytest.mark.parametrize("scenario, expected_result", [
-    ("visible", "PASS"),
-    ("absent", "FAIL"),
-    ("hidden", "FAIL"),
-])
-def test_first_name_field(page_open, scenario, expected_result, user_data):
-    if scenario == "visible":
-        # user_data = request.getfixturevalue("user_data")
-        # используешь user_data только тут
-        with allure.step('перехід на посилання створення екаунту та клік на ньому'):
-            # Перевірка наявності по тексту
-            # expect(page_open.get_by_text("Create an Account")).to_be_visible(timeout=30000)
-            link = page_open.get_by_role("link", name="Create an Account")
-            # expect(page_open.get_by_role("link", name="Create an Account")).to_be_visible(timeout=40000)
-            expect(link).to_be_visible(timeout=40000)
-            debug("перехід на посилання створення екаунту", "Посилання створення екаунту")
-            # page_open.get_by_text("Create an Account").click()
-            # page_open.get_by_role("link", name="Create an Account").click()
-            link.click()
-            # page_open.get_by_text("Create New Customer Account").click()
-            debug("клік на посиланні створення екаунту", "Посилання створення екаунту")
+# @allure.title("Позитивний тест: поле 'First Name*' відображається")
+# @pytest.mark.parametrize("scenario, expected_result", [
+#     ("visible", "PASS"),
+#     ("absent", "FAIL"),
+#     ("hidden", "FAIL"),
+# ])
+# def test_first_name_field(page_open, scenario, expected_result, user_data):
+#     page_open.goto("https://demoqa.com/")
+#     import re
+#     from playwright.sync_api import Page, expect
 
-            # --- обхід реклами ---
-            # Проверяем и закрываем рекламу
+def test_example(page_open: Page, user_data) -> None:
+    page_open.locator("div").filter(has_text=re.compile(r"^Elements$")).first.click()
+    page_open.get_by_text("Book Store Application").click()
+    page_open.get_by_role("listitem").filter(has_text="Login").click()
+    page_open.get_by_role("button", name="New User").click()
+    page_open.get_by_role("textbox", name="First Name").click()
+    page_open.get_by_role("textbox", name="Last Name").click()
+    page_open.get_by_role("textbox", name="UserName").click()
+    page_open.get_by_role("textbox", name="Password").click()
+    page_open.locator("iframe[name=\"a-yvxz48f9weee\"]").content_frame.get_by_role("checkbox",
+                                                                              name="I'm not a robot").click()
 
-            # Если попали на рекламу — возвращаемся назад
-            if "google_vignette" in page_open.url or "ad.doubleclick" in page_open.url:
-                debug("Виявлено рекламу google_vignette. Повертаємось назад...", "WARNING")
-                page_open.go_back()
-                expect(link).to_be_visible(timeout=20000)
-                link.click()
-                debug("повторний клік після реклами", "INFO")
-            # close_button = page_open.locator("text=Close").first
-            close_button = page_open.get_by_role("button", name="Close").first
-            if close_button.is_visible():
-                close_button.click()
-            # if page_open.locator("text=Close").is_visible():
-            #     page_open.locator("text=Close").click()
-            # if "google_vignette" in page_open.url:
-            #     debug("Виявлено рекламу google_vignette. Повертаємось назад...", "WARNING")
-            #     page_open.go_back()
-            #     expect(link).to_be_visible(timeout=20000)
-            #     link.click()
-            #     debug("повторний клік після реклами", "INFO")
-
-        with allure.step('перевірка заголовку, чи це сторінка створення екаунту'):
-            # page_cr_ac = page_open.get_by_role("heading", name="Create New Customer Account")
-            expect(page_open.get_by_role("heading")).to_contain_text("Create New Customer Account", timeout=40000)
-            debug("здійснено перехід на сторінку створення екаунту", "Сторінка створення екаунту")
-    # if scenario == "visible":
-        allure.dynamic.title(f"Позитивний тест: поле 'First Name*' відображається → {expected_result}")
-        with allure.step("пошук і перехід на поле імені та введення його"):
-            expect(page_open.get_by_role("textbox", name="First Name*")).to_be_visible()
-            page_open.get_by_role("textbox", name="First Name*").fill(user_data['login'])
-            debug("знайдено та заповнено поле імені", "Поле імені")
-
-    elif scenario == "absent":
-        allure.dynamic.title(f"Негативний тест: поле 'First Name*' відсутнє у DOM → {expected_result}")
-        with allure.step("перевірка відсутності елемента поля імені"):
-            # expect(page_open.get_by_role("textbox", name="First Name*")).to_have_count(0)
-            expect(page_open.get_by_role("textbox", name="First Name*")).to_have_count(0)
-            debug("поле імені відсутнє у DOM", "Поле імені")
-
-    elif scenario == "hidden":
-        allure.dynamic.title(f"Негативний тест: поле 'First Name*' приховане → {expected_result}")
-        with allure.step("перевірка що поле імені приховане"):
-            expect(page_open.get_by_role("textbox", name="First Name*")).to_be_hidden()
-            # expect(page_cr_ac.get_by_role("textbox", name="First Name*")).to_be_hidden()
-            debug("поле імені є у DOM, але приховане", "Поле імені")
+    page_open.get_by_role("button", name="Register").click()
 
