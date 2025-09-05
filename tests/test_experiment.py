c02d747b1247939ede6fd4bd70d73990d515f08f
Serhii Kotliar <arecserg@gmail.com>
2025-09-05 09:19:43 +0300

    demoqa

--- tests/test_experiment.py
+++ tests/test_experiment.py
@@ -21,12 +21,21 @@ def test_example(page_open: Page, user_data) -> None:
     page_open.get_by_text("Book Store Application").click()
     page_open.get_by_role("listitem").filter(has_text="Login").click()
     page_open.get_by_role("button", name="New User").click()
-    page_open.get_by_role("textbox", name="First Name").click()
-    page_open.get_by_role("textbox", name="Last Name").click()
-    page_open.get_by_role("textbox", name="UserName").click()
-    page_open.get_by_role("textbox", name="Password").click()
-    page_open.locator("iframe[name=\"a-yvxz48f9weee\"]").content_frame.get_by_role("checkbox",
-                                                                              name="I'm not a robot").click()
-
+    page_open.get_by_role("textbox", name="First Name").fill(user_data[0]['login'])
+    page_open.get_by_role("textbox", name="Last Name").fill(user_data[0]['login_l'])
+    page_open.get_by_role("textbox", name="UserName").fill(user_data[0]['email'])
+    page_open.get_by_role("textbox", name="Password").fill(user_data[0]['password'])
+    # Перехватываем запрос к серверу reCAPTCHA и возвращаем успешный ответ
+    page_open.route("https://www.google.com/recaptcha/api2/**", lambda route: route.fulfill(
+        status=200,
+        content_type="application/json",
+        body='{"success": true}'
+    ))
+    # expect(page_open.locator("iframe[name=\"a-58bd9sn74qxo\"]").content_frame.locator(".rc-anchor-center-item").first).to_be_visible()
+    # page_open.locator("iframe[name=\"a-58bd9sn74qxo\"]").content_frame.locator(".rc-anchor-center-item").first.click()
+    expect(page_open.get_by_role("button", name="Register")).to_be_visible()
     page_open.get_by_role("button", name="Register").click()
+
+
+
 
